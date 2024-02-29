5da732a666837e3567e8296d25417ec38c0237e2
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2024-02-29 15:27:46 -0300

    feat: criação de tela de fundo inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartQuiz from './components/StartQuiz';
 
 function App() {
   return (
     <div className="App">
-     
+    <StartQuiz></StartQuiz>
     </div>
   );
 }
