39c265a6db144fa99a7d0df713ebd5e8d4e67c9b
Frankz1n <franklyn.lima016@gmail.com>
2024-03-01 11:29:42 -0300

    deixei o modal mais responsivo para telas grandes e medias

--- src/components/ModalOne/index.js
+++ src/components/ModalOne/index.js
@@ -3,48 +3,30 @@ import "./ModalOne.css";
 const ModalOne = ({ isOpen }) => {
   if (isOpen) {
     return (
-      <div className="backgorund-modal">
+      <div className="background">
         <div className="modal">
-          <h1>Escolha sua comida preferida</h1>
-          <div className="modal-div">
-            <button onClick={} className="btn-modal">
-              <h2>Pizza</h2>
-              <img
-                src="/imgs/modal1/pizza.png"
-                alt="imagem de uma pizza"
-                className="modal-img"
-              ></img>
+          <h2>Escolha sua comida preferida</h2>
+          <div className="img-divs">
+            <button>
+              <img src="/imgs/modal1/pizza.png"></img>
             </button>
-            <button className="btn-modal">
-              <h2>Hamburguer</h2>
-              <img
-                src="/imgs/modal1/hamburguer.png"
-                alt="imagem de um hamburguer"
-                className="modal-img"
-              ></img>
+            <button>
+              <img src="/imgs/modal1/estrogonofe.png"></img>
             </button>
           </div>
-          <div className="modal-div">
-            <button className="btn-modal">
-              <h2>Massa</h2>
-              <img
-                src="/imgs/modal1/massa.png"
-                alt="imagem de uma massa"
-                className="modal-img"
-              ></img>
+          <div className="img-divs">
+            <button>
+              <img src="/imgs/modal1/massa.png"></img>
             </button>
-            <button className="btn-modal">
-              <h2>Estrogonofe</h2>
-              <img
-                src="/imgs/modal1/estrogonofe.png"
-                alt="imagem de um estrogonofe"
-                className="modal-img"
-              ></img>
+            <button>
+              <img src="/imgs/modal1/hamburguer.png"></img>
             </button>
           </div>
         </div>
       </div>
     );
+  } else {
+    return <></>;
   }
 };
 
