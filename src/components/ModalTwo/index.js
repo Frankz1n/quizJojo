04fb1fd97d9e7a5f71382b65732966529a70a5da
Frankz1n <franklyn.lima016@gmail.com>
2024-03-04 13:59:12 -0300

    fix: responsive settings in modal style

--- src/components/ModalTwo/index.js
+++ src/components/ModalTwo/index.js
@@ -1,20 +1,20 @@
 import OptionQuestionButton from "../OptionQuestionButton";
-import "./ModalTwo.css";
+import "../ModalOne/ModalOne.css";
 
 const ModalTwo = ({ handleSelectQuestion }) => {
-    return (
-      <div className="backgorund-modal">
+  return (
+    <div className="backgorund-modal">
       <div className="modal">
         <h1>Escolha sua comida preferida</h1>
         <div className="modal-div">
           <OptionQuestionButton
-            title={'Pizza2'}
+            title={"Pizza2"}
             img={"/imgs/modal1/pizza.png"}
             alt={"imagem de uma pizza"}
             onClick={() => handleSelectQuestion("optionOne", 3)}
           />
           <OptionQuestionButton
-            title={'Hamburguer2'}
+            title={"Hamburguer2"}
             img={"/imgs/modal1/hamburguer.png"}
             alt={"imagem de um hamburguer"}
             onClick={() => handleSelectQuestion("optionTwo", 3)}
@@ -22,13 +22,13 @@ const ModalTwo = ({ handleSelectQuestion }) => {
         </div>
         <div className="modal-div">
           <OptionQuestionButton
-            title={'Massa2'}
+            title={"Massa2"}
             img={"/imgs/modal1/massa.png"}
             alt={"imagem de uma massa"}
             onClick={() => handleSelectQuestion("optionThree", 3)}
           />
           <OptionQuestionButton
-            title={'Estrogonofe2'}
+            title={"Estrogonofe2"}
             img={"/imgs/modal1/estrogonofe.png"}
             alt={"imagem de um estrogonofe"}
             onClick={() => handleSelectQuestion("optionFour", 3)}
@@ -36,7 +36,7 @@ const ModalTwo = ({ handleSelectQuestion }) => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default ModalTwo;
--- src/components/OptionQuestionButton/index.js
+++ src/components/OptionQuestionButton/index.js
@@ -1,15 +1,12 @@
-const OptionQuestionButton = ({title, img, alt, onClick}) => {
+import "../ModalOne/ModalOne.css";
 
+const OptionQuestionButton = ({ title, img, alt, onClick }) => {
   return (
     <button onClick={onClick} className="btn-modal">
       <h2>{title}</h2>
-      <img
-        src={img}
-        alt={alt}
-        className="modal-img"
-      />
-  </button>
-  )
-}
+      <img src={img} alt={alt} className="modal-img" />
+    </button>
+  );
+};
 
-export default OptionQuestionButton
+export default OptionQuestionButton;
