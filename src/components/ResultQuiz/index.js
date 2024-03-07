613d1868363679f92b5ade216f19fcf51d4f3b2d
Frankz1n <franklyn.lima016@gmail.com>
2024-03-07 14:26:18 -0300

    feat: create button restart quiz

--- src/components/ResultQuiz/index.js
+++ src/components/ResultQuiz/index.js
@@ -1,31 +1,29 @@
-import ModalResult from "../ModalResult"
+import ModalResult from "../ModalResult";
 
 const ResultQuiz = ({ answer }) => {
-
-  let answers = Object.keys(answer)
-  let mostChosenQuestion = ''
-  let min = 2
-  let neutralChoice = 0
+  let answers = Object.keys(answer);
+  let mostChosenQuestion = "";
+  let min = 2;
+  let neutralChoice = 0;
 
   answers.forEach((element) => {
     if (answer[element] >= min) {
       if (min === answer[element]) {
-        neutralChoice++
+        neutralChoice++;
       }
-      mostChosenQuestion = element
-      min = answer[element]
+      mostChosenQuestion = element;
+      min = answer[element];
     }
-  })
+  });
 
-  let optionFive = neutralChoice >= 2 ? true : false
+  let optionFive = neutralChoice >= 2 ? true : false;
 
   return (
     <ModalResult
       mostChosenQuestion={mostChosenQuestion}
       optionFive={optionFive}
     />
-  )
+  );
+};
 
-}
-
-export default ResultQuiz
+export default ResultQuiz;
